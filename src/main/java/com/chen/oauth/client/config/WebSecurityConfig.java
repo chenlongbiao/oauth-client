aabb7743bd90845f33158365bf0d640026a98c64
chenlongbiao <chenlongbiao-cf>
2019-03-15 09:49:58 +0800

    修改解析角色信息规则

--- src/main/java/com/chen/oauth/client/config/WebSecurityConfig.java
+++ src/main/java/com/chen/oauth/client/config/WebSecurityConfig.java
@@ -35,8 +35,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
                 // 对于获取token的rest api要允许匿名访问
-                .antMatchers("/token").permitAll()
-
+                .antMatchers("/token","/blog/core")
+                .permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated();
         http
